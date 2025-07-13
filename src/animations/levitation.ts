d4f0628794e9b46cc0e93bd06663c3cace5afae4
GuilhermeAMendes <guilherme.andmendes03@gmail.com>
2025-07-12 23:12:16 -0300

    feat: create package from animations with framer motion

---
+++ src/animations/levitation.ts
@@ -0,0 +1,11 @@
+// External library
+import { easeInOut } from "framer-motion";
+
+export const LEVITATION = {
+  animate: { y: [0, -15, 0] },
+  transition: {
+    duration: 1.5,
+    repeat: Infinity,
+    ease: easeInOut,
+  },
+};
